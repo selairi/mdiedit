500bb1fb5711422d8fca6369cc436b35c4a92160
lucas <selairi@gmail.com>
2014-08-08 08:23:32 +0200

    Added Main window icon.

--- mdiedit/main.cpp
+++ mdiedit/main.cpp
@@ -58,6 +58,7 @@ int main(int argn, char *argv[])
 		fileList << argv[i];
 	}
 	MainWindow mainWin;
+	mainWin.setWindowIcon(QIcon(ICON_PATH "/mdiedit.svg"));
 	mainWin.show();
 	QString file;
 	foreach(file, fileList)
